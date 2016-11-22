6b2a26b26186dfa9b8f7da64f9d50b5b216ed9c2
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-11-22 11:05:36 +0000

    Tidy up requires

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,12 +19,12 @@ class ekahau_throughput_server (
     destination => "${installdir}/iperf3-ekahau.zip",
     timeout     => 0,
     verbose     => false,
-    notify      => Exec['unzip'],
+    notify      => Exec['unzip-ekahau'],
     require     => File[$installdir],
   }
 
   # unzip it
-  exec { 'unzip':
+  exec { 'unzip-ekahau':
     command     => 'unzip iperf3-ekahau.zip',
     cwd         => $installdir,
     path        => '/usr/bin',
@@ -34,14 +34,17 @@ class ekahau_throughput_server (
   # Configure a systemd unit
   ::systemd::unit_file { 'ekahauiperf.service':
     source  => "puppet:///modules/${module_name}/ekahauiperf.service",
-    require => Class['java'],
   }
 
   # start service
   service { 'ekahauiperf':
     ensure  => 'running',
-    enable  => 'true',
-    require => Systemd::Unit_file['ekahauiperf.service'],
+    enable  => true,
+    require => [
+      Systemd::Unit_file['ekahauiperf.service'],
+      Exec['unzip-ekahau'],
+      Class['java'],
+    ],
   }
 
   # make firewall exception
