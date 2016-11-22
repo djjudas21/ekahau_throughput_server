9edaf8e979a0c9939fb67bfdcf15b34e549ffc4c
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-11-22 10:54:39 +0000

    Remove useless comment - it's all in the readme

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,47 +1,4 @@
-# Class: ekahau_throughput_server
-# ===========================
-#
 # Deploy Ekahau Throughput Server on Linux
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'ekahau_throughput_server':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2016 Your name here, unless otherwise noted.
-#
 class ekahau_throughput_server (
   $url = 'http://www.ekahau.com/userData/ekahau/wifi-design/documents/iperf3-ekahau.zip',
   $installdir = '/opt/ekahau_throughput_server',
